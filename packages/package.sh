4d6947a4e7f31b56fa618f767b23da9813f2d925
siddharthist <langston.barrett@gmail.com>
2015-06-23 12:04:16 -0500

    Reduced binary size ~2MB with compiler flags

--- packages/package.sh
+++ packages/package.sh
@@ -1,1 +1,2 @@
-/home/lb/.gem/ruby/2.2.0/bin/fpm -s dir -t rpm -n distributive -f -d go -m langston@aster.is -v 0.1.1 --epoch 0 ../distributive=/usr/bin/ ../samples=/etc/distributive.d/samples/
+go build -ldflags "-w -s" ../ &&
+~/.gem/ruby/2.2.0/bin/fpm -s dir -t rpm -n distributive -f -d go -m langston@aster.is -v 0.1.2 --epoch 0 ../distributive=/usr/bin/ ../samples=/etc/distributive.d/samples/
