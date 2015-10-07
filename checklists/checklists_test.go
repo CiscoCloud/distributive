4f11f08a5dc07e2da2217f324ec5403598fa9800
siddharthist <langston.barrett@gmail.com>
2015-10-06 19:08:50 -0700

    Added fuzzing script Haven't found bugs yet! Fixes #71

--- checklists/checklists_test.go
+++ checklists/checklists_test.go
@@ -37,30 +37,3 @@ func TestChecklistFromURL(t *testing.T) {
 	t.Parallel()
 	// TODO
 }
-
-/************************* FUZZING *************************/
-/* https://github.com/dvyukov/go-fuzz */
-
-func partiallyValid(chklst Checklist) bool {
-	// are any of the fields not zero valued?
-	switch {
-	case len(chklst.Checks) != 0:
-		return true
-	case chklst.Name != "":
-		return true
-	case chklst.Notes != "":
-		return true
-	case chklst.Origin != "":
-		return true
-	}
-	return false
-}
-
-func Fuzz(data []byte) int {
-	chklst, err := ChecklistFromBytes(data)
-	if err == nil || partiallyValid(chklst) {
-		return 0
-	} else {
-		return 1
-	}
-}
---
+++ fuzz.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# fail if any steps fail
+# Install the necessary tools
+if [[ ! $(hash go-fuzz-build 2>/dev/null) ]]; then
+  echo >&2 "You have go-fuzz-build! Continuing..."
+else
+  echo >&2 "Installing go-fuzz-build..."
+  go get github.com/dvyukov/go-fuzz/go-fuzz
+  go get github.com/dvyukov/go-fuzz/go-fuzz-build
+fi
+
+# append Fuzzing method to end of checklists.go
+read -r -d '' to_append <<'EOF'
+
+func Fuzz(data []byte) int {
+	_, err := ChecklistFromBytes(data)
+	if err == nil {
+		return 0
+	}
+	return 1
+}
+EOF
+
+cp checklists/checklists.go{,.bak} # save checklists.go
+echo "$to_append" >> "checklists/checklists.go"
+go-fuzz-build github.com/CiscoCloud/distributive/checklists
+mkdir fuzzing/corpus
+cp samples/*.json fuzzing/corpus
+go-fuzz	-bin="checklists-fuzz.zip" -workdir=fuzzing
+cp checklists/checklists.go{.bak,} # restore checklists.go
+rm checklists/checklists.go.bak    # remove backup
