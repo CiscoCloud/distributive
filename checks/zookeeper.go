6f128c69982c5f71eefbf88777845dc944e4902d
Langston Barrett <langston.barrett@gmail.com>
2016-04-13 12:31:31 -0700

    Add zk latency check

--- checks/zookeeper.go
+++ checks/zookeeper.go
@@ -1,6 +1,7 @@
 package checks
 
 import (
+	"errors"
 	"time"
 
 	"github.com/CiscoCloud/distributive/chkutil"
@@ -54,4 +55,59 @@ func (chk ZooKeeperRUOK) Status() (int, string, error) {
 		return errutil.Success()
 	}
 	return 1, "Failed: " + failed, nil
+}
+
+/*
+#### ZooKeeperLatency
+Description: Is the latency of these zookeeper servers below this value?
+Parameters:
+- Max latency (time.Duration): Maximum acceptable average latency
+- Servers ([]string): List of zookeeper servers
+Example parameters:
+- "5s", "20ms", "2h"
+- "localhost:2181", "zookeeper.service.consul:2181"
+*/
+type ZooKeeperLatency struct {
+	maxLatency time.Duration
+	servers    []string
+}
+
+func (chk ZooKeeperLatency) ID() string { return "ZooKeeperLatency" }
+
+func (chk ZooKeeperLatency) New(params []string) (chkutil.Check, error) {
+	if len(params) < 2 {
+		return chk, errutil.ParameterLengthError{2, params}
+	}
+	dur, err := time.ParseDuration(params[0])
+	if err != nil {
+		return chk, errutil.ParameterTypeError{params[0], "time.Duration"}
+	}
+	chk.maxLatency = dur
+	chk.servers = params[1:]
+	return chk, nil
+}
+
+func (chk ZooKeeperLatency) Status() (int, string, error) {
+	stats, ok := zk.FLWSrvr(chk.servers, chk.maxLatency)
+	if !ok {
+		if len(stats) > 1 {
+			for _, srv := range stats {
+				if srv.Error != nil {
+					return 1, "", srv.Error
+				}
+			}
+		}
+		return 1, "", errors.New("Unknown error in zk api")
+	}
+	var failed string
+	// match zookeeper servers with failures for error message
+	for i, stat := range stats {
+		if stat.AvgLatency > int64(chk.maxLatency) {
+			failed += chk.servers[i]
+		}
+	}
+	if failed == "" {
+		return errutil.Success()
+	}
+	return 1, "Latency too high: " + failed, nil
 }
--- checks/zookeeper_test.go
+++ checks/zookeeper_test.go
@@ -4,23 +4,24 @@ import (
 	"testing"
 )
 
+var validInputs = [][]string{
+	[]string{"2ms", "wikipedia.org:9814"},
+	[]string{"1ms", "mozilla.org:9814"},
+}
+
+var invalidInputs = [][]string{
+	{"", "mozilla.net"},
+	{"nottime", "wikipedia.org"},
+}
+
 func TestZooKeeperRUOK(t *testing.T) {
 	t.Parallel()
-	if testing.Short() {
-		t.Skip("Skipping docker tests in short mode")
-	} else {
-		validInputs := [][]string{
-			[]string{"2ms", "wikipedia.org:9814"},
-			[]string{"1ms", "mozilla.org:9814"},
-		}
-		invalidInputs := [][]string{
-			{"", "mozilla.net"},
-			{"nottime", "wikipedia.org"},
-		}
-		// inputs that should lead to success
-		goodEggs := [][]string{}
-		// inputs that should lead to failure
-		testParameters(validInputs, invalidInputs, ZooKeeperRUOK{}, t)
-		testCheck(goodEggs, validInputs, ZooKeeperRUOK{}, t)
-	}
+	testParameters(validInputs, invalidInputs, ZooKeeperRUOK{}, t)
+	testCheck([][]string{}, validInputs, ZooKeeperRUOK{}, t)
+}
+
+func TestZooKeeperLatency(t *testing.T) {
+	t.Parallel()
+	testParameters(validInputs, invalidInputs, ZooKeeperLatency{}, t)
+	testCheck([][]string{}, validInputs, ZooKeeperLatency{}, t)
 }
